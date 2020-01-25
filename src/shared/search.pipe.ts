98d7229588490440f0f9b6ced1cd846c8046f152
Yogesh <yogdeep.k@gmail.com>
2020-01-25 23:47:42 +0530

    case sensitive search result removed.

--- src/shared/search.pipe.ts
+++ src/shared/search.pipe.ts
@@ -3,13 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'titleFilter'
 })
 export class SearchPipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
     if (!args) {
       return value;
     }
     return value.filter((val) => {
-      const rVal = (val.title.toLocaleLowerCase().includes(args));
+      const rVal = (val.title.toLocaleLowerCase().includes(args.toLowerCase()));
       return rVal;
     });
 
